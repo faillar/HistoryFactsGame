fd5e75a2f4b0baf85c77f6826c5fce9564d0a79a
RuFFuS4 <rgr93.4@outlook.es>
2018-05-13 21:11:36 +0200

    Create the first Network Conecction
    He empezado a usar Network para crear clientes para que se conecte más gente (es un primer vistazo y prueba)

---
+++ History Facts - The Game/Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Since the PlayerObject is invisible and not part of the world, give me something physical to move around!
+        Instantiate(PlayerUnitPrefab);
+	}
+    public GameObject PlayerUnitPrefab;
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
